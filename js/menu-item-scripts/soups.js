1760da5e958c30f5f9edd33e2675b22677b54052
Mark Saville <mark.saville@outlook.com>
2024-03-27 19:52:23 +0100

    Add soups.js file to dynamically update menu with soup items

---
+++ js/menu-item-scripts/soups.js
@@ -0,0 +1,51 @@
+// Number of menu items to be added:
+const numberOfMenuItems = 11;
+
+// Function to generate a random price within a specified range
+function getRandomPrice(min, max) {
+  return (Math.random() * (max - min) + min).toFixed(2);
+}
+
+// Function to update the restaurant menu
+function updateMenu(numberOfMenuItems) {
+  const menuContainer = document
+    .getElementById("main-content")
+    .querySelector("section");
+
+  // Clear existing menu items
+  menuContainer.innerHTML = "";
+
+  // Loop to generate specified number of menu items
+  for (let i = 1; i <= numberOfMenuItems; i++) {
+    // Generate random prices between specified range
+    const priceSmall = `$${getRandomPrice(10, 15)}`;
+    const priceLarge = `$${getRandomPrice(12, 18)}`;
+
+    const menuItemHTML = `
+      <div class="menu-item-tile col-md-6">
+        <div class="row">
+          <div class="col-sm-5">
+            <div class="menu-item-photo">
+              <div>D${i.toString().padStart(2, "0")}</div>
+              <img class="img-responsive" width="250" height="150" src="/images/menu-item-tiles/soups/soups (${i}).jpg" alt="Item" />
+            </div>
+            <div class="menu-item-price">
+              ${priceSmall}<span> (small)</span> ${priceLarge} <span>(large)</span>
+            </div>
+          </div>
+          <div class="menu-item-description col-sm-7">
+            <h3 class="menu-item-title">D${i.toString().padStart(2, "0")}</h3>
+            <p class="menu-item-details">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque inventore esse minima incidunt impedit. Asperiores, voluptatem. Sint aspernatur provident, rem odio dolorem eaque voluptatibus modi reprehenderit minima, itaque cupiditate totam.</p>
+          </div>
+        </div>
+        <hr class="visible-xs" />
+      </div>
+    `;
+
+    // Append the menu item HTML to the container
+    menuContainer.innerHTML += menuItemHTML;
+  }
+}
+
+// Call the function to update the menu with 11 items
+updateMenu(numberOfMenuItems);
